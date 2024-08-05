63b23133f95bfcd95ff3875ec8da1c0f1e1495b9
Philip Coomber <philipcoomber@gmail.com>
2024-08-05 21:06:09 +0200

    fix: append new values to market type and maintain ids

--- jobs/clean/market_type.py
+++ jobs/clean/market_type.py
@@ -2,6 +2,7 @@ import argparse
 
 from pyspark.sql import SparkSession
 from pyspark.sql import functions as F
+from pyspark.sql.window import Window
 
 from shared.common import get_flattened_df, setup_spark_environment
 
@@ -10,11 +11,19 @@ def save(namespace: str, branch: str):
     spark: SparkSession = setup_spark_environment(namespace, branch)
 
     flattened_df = get_flattened_df(spark)
+    old_market_type = spark.read.table("market_type").alias("old")
+    w = Window.partitionBy().rowsBetween(Window.unboundedPreceding, Window.currentRow)
     market_type = (
         flattened_df.select(F.col("marketType").alias("type"))
-        .distinct()
-        .withColumn("id", F.monotonically_increasing_id())
-        .select(F.col("id"), F.col("type"))
+            .alias("new")
+            .distinct()
+            .join(old_market_type, F.col("old.type") == F.col("new.type"), how="left")
+            .orderBy(F.col("old.id").asc_nulls_last())
+            .withColumn(
+                "new_id", F.sum(F.when(F.col("old.id") == F.lit(0), 0).otherwise(1)).over(w)
+            )
+            .filter(F.isnull(F.col("old.id")))
+            .select(F.col("new_id").alias("id"), F.col("new.type").alias("type"))
     )
 
     market_type.write.format("iceberg").mode("append").save("market_type")
