8816f2cde957ffcc1bea27d7a58e6793e54c4bdb
Philip Coomber <philipcoomber@gmail.com>
2025-02-02 13:28:27 +0100

    fix: market_type column name

--- jobs/clean/market_type.py
+++ jobs/clean/market_type.py
@@ -17,13 +17,13 @@ def save(namespace: str, branch: str):
         spark.table("betting.landing.raw")
         .filter(F.col("mc.marketDefinition").isNotNull())
         .select(
-            F.col("mc.marketDefinition.marketType"),
+            F.col("mc.marketDefinition.marketType").alias("type"),
         )
         .distinct()
         .withColumn("id", F.monotonically_increasing_id())
     )
 
-    save_table(spark, market_type, f"{namespace}.market_type", mode=WriteMode.APPEND)
+    save_table(spark, market_type, f"{namespace}.market_type", mode=WriteMode.REPLACE)
 
 
 if __name__ == "__main__":
